5cd5d2f82b29ac10bea99d1eaaac24568bffce24
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-01-24 16:01:03 +0530

    Create sam54.C

---
+++ sam54.C
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+void main()
+{
+    int N;
+    printf("ENTER ANY VALUE");
+    scanf("\t%d",&N);
+    N-=1;
+    if(N%2==0)
+    printf("%d",N);
+    else
+    {
+    N-=1;
+    printf("%d",N);
+}
+        
+    }
+
+    
